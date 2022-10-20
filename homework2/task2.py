c8c19f26360ceb8ad747ccb612c67c760b4ce10e
Sveta Martynova <svetamart95@gmail.com>
2022-10-21 00:52:30 +0300

    changed hw2 task 2

--- homework2/task2.py
+++ homework2/task2.py
@@ -3,7 +3,11 @@
 # пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
 n = int(input('Введите положительное число N: '))
+
+lst = []
 a = 1
-for i in range(a, n + 1):
+for i in range(1, n + 1):
     a *= i
-    print(a, end=' ')
+    lst.append(a)
+
+print(lst)
